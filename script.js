6633bcc9444da3a40941e969d39838493ca60532
Robert Blaylock <rblaylock.prsvr@gmail.com>
2024-04-30 20:13:42 -0500

    chaged code and added a button

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 let firstCard = 10
 let secondCard = 11
-let sum = firstCard + secondCard + 4
+let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
-
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame(){
+    sumEl.textContent = "Sum: " + sum
+cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
 
 if (sum <= 20) {
-    message = "Do you want to draw a new card? "
+    message = "Do you want to draw a new card?"
 } else if (sum === 21) {
-    message = "wohoo! You've got Blackjack! 🥳"
+    message = "You've got Blackjack!"
     hasBlackJack = true
 } else {
-    message = "You're out of the game! 😵"
+    message = "You're out of the game!"
     isAlive = false
 }
 
-console.log(message)
+messageEl.textContent = message
+}
 
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+}
 
 
 
